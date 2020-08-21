8f4750f23a38f5d6f5aa00844f63b51625dd1210
Gian Pumayalla <gian.pumayalla@bene.it>
2020-08-21 17:34:56 +0200

    Init repository

---
+++ jquery.domstore.js
@@ -0,0 +1,8 @@
+$$ = (function($) {
+    $DOMSTORE = {};
+    return function(selector, force) {
+        if ($DOMSTORE[selector] === undefined || force)
+            $DOMSTORE[selector] = $(selector);
+        return $DOMSTORE[selector];
+    }
+})($);
